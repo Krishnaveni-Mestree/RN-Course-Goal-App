39607944dda1561df11402a9c310371e1852aac9
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-06-06 18:05:59 +0530

    handled inputField and Button events

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredText,setEnteredText]=useState('');
+
+  function goalInputHandler(enteredText){
+    //console.log(enteredText);
+    setEnteredText(enteredText)
+  };
+
+  function addGoalHandler(){
+    console.log(enteredText);
+  };
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput 
           placeholder='Your course goal!'
           style={styles.textInput} 
+          onChangeText={goalInputHandler} //we r not executing this function, just pointing(focusing) the inputfield
         />
-        <Button title="Add Goal"/>
+        <Button 
+          title="Add Goal"
+          onPress={addGoalHandler}
+        />
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of Goals....</Text>
